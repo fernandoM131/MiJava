fe1a2c3c4f1d12e71d082b4848eb229d28fb766c
Fernando <fernando.martinez299@std.uni.edu.ni>
2025-06-25 13:02:52 -0600

    cambio confirmado

---
+++ HolaMundo.java
@@ -0,0 +1,14 @@
+package Miproyecto;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        String nombre = "Estudiante";
+        int a = 10;
+        int b = 9;
+        int suma = a + b;
+
+        System.out.println("Hola, " + nombre + "!");
+        System.out.println("La suma de " + a + " + " + b + " es: " + suma);
+    }
+}
+
